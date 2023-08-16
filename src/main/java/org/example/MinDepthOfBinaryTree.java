6b09fe0deb3ab7c6cde73fba568b2661ea3b75f2
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-16 11:25:55 +0300

    Minimum Depth of Binary Tree

---
+++ src/main/java/org/example/MinDepthOfBinaryTree.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class MinDepthOfBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int minDepth(TreeNode root) {
+        return root == null ? 0 : depth(root);
+    }
+
+    private int depth(TreeNode current) {
+        if (current == null) return Integer.MAX_VALUE; // to prevent selection
+        if(current.left == null && current.right == null) return 1;
+        return 1 + Math.min(depth(current.left), depth(current.right));
+    }
+}
