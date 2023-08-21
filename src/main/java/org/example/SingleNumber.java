4ddd80c46faa50f97b20e61568231f6859f1361c
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-21 12:03:06 +0300

    Find single number

--- src/main/java/org/example/SingleNumber.java
+++ src/main/java/org/example/SingleNumber.java
@@ -1,2 +1,11 @@
-package org.example;public class SingleNumber {
+package org.example;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for (int num : nums) {
+            singleNumber ^= num;
+        }
+        return singleNumber;
+    }
 }
