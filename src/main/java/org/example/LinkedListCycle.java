247ac73b529a2acb6574ae9d56988ac5103e2540
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-22 11:40:26 +0300

    Find if LinkedList has a cycle

---
+++ src/main/java/org/example/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LinkedListCycle {
+
+    //Definition for singly-linked list.
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+
+        public static boolean hasCycle(ListNode head) {
+            Set<ListNode> nodeSet = new HashSet<>();
+            while (head != null) {
+                if(!nodeSet.add(head)) return true;
+                head = head.next;
+            }
+            return false;
+        }
+    }
+}
