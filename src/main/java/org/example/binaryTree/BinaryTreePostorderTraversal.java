597f57f1d1ed0d67608036789dcdcef4d4dd1cf9
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-24 10:59:34 +0300

    Perform postrder traversal of BinaryTree

---
+++ src/main/java/org/example/binaryTree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,37 @@
+package org.example.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostorderTraversal {
+    // Definition for a binary tree node.
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> btList = new ArrayList<>();
+
+        if (root != null){
+            btList.addAll(postorderTraversal(root.left));
+            btList.addAll(postorderTraversal(root.right));
+            btList.add(root.val);
+        }
+        return btList;
+    }
+}
