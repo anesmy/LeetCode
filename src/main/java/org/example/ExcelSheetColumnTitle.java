f39bb868ba868865069dcc7d68e838c67a984b3e
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-08-26 12:58:00 +0300

    Find the corresponding Excel sheet column title

---
+++ src/main/java/org/example/ExcelSheetColumnTitle.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ExcelSheetColumnTitle {
+    private static final char START = 64;
+
+    public static String convertToTitle(int columnNumber) {
+        StringBuilder title = new StringBuilder();
+
+        for(; columnNumber > 26; columnNumber /= 26){
+            title.append(getLetter(columnNumber % 26));
+            if (columnNumber % 26 == 0) columnNumber--;
+        }
+        title.append(getLetter(columnNumber));
+        return title.reverse().toString();
+    }
+
+    private static char getLetter(int num) {
+        if (num == 0) num = 26;
+        return (char) (START + num);
+    }
+}
