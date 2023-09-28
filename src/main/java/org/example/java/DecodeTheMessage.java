304297aa9539a18b547afe18c1dbba1df8938e01
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-09-28 11:18:38 +0300

    Clean code

--- src/main/java/org/example/java/DecodeTheMessage.java
+++ src/main/java/org/example/java/DecodeTheMessage.java
@@ -1,12 +1,10 @@
 package org.example.java;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class DecodeTheMessage {
-    public static String decodeMessage(String key, String message) {
+    public String decodeMessage(String key, String message) {
         List<Character> alphabet = createAlphabet(key);
         return message.chars()
                 .mapToObj(ch -> (char) ch)
@@ -21,9 +19,5 @@ public class DecodeTheMessage {
                 .distinct()
                 .mapToObj(ch -> (char) ch)
                 .collect(Collectors.toList());
-    }
-
-    public static void main(String[] args) {
-        System.out.println(decodeMessage("the quick brown fox jumps over the lazy dog", "vkbs bs t suepuv"));
     }
 }
