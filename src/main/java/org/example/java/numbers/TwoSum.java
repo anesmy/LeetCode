096bf19af022c6e5f0f0e2cddd66c1603d71e80c
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-11-02 13:00:53 +0200

    Two Sum

---
+++ src/main/java/org/example/java/numbers/TwoSum.java
@@ -0,0 +1,18 @@
+package org.example.java.numbers;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length && i != j; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+}
