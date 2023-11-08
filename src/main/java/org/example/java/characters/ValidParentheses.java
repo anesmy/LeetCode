266ac8e60d621bc235354c641cf0fee034d72779
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-11-08 09:25:40 +0200

    Valid Parentheses

---
+++ src/main/java/org/example/java/characters/ValidParentheses.java
@@ -0,0 +1,27 @@
+package org.example.java.characters;
+
+
+import java.util.Stack;
+
+
+public class ValidParentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+
+            switch (c) {
+                case '{' -> stack.add('}');
+                case '[' -> stack.add(']');
+                case '(' -> stack.add(')');
+                default -> {
+                    if (stack.empty() || stack.pop() != c) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return stack.empty();
+    }
+}
+
+// https://leetcode.com/problems/valid-parentheses/description/
